e3b1532aae928e64d21a2278aa00b695410094b9
Hirofum Hayashi <pica.hayashi@gmail.com>
2022-03-29 10:57:08 +0900

    Add Japanese translation file

---
+++ i18n/mapswipetool_ja_JP.ts
@@ -0,0 +1,32 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="ja_JP" sourcelanguage="en_US">
+<context>
+    <name>MapSwipeTool</name>
+    <message>
+        <location filename="../mapswipetool.py" line="97"/>
+        <source>Select Layer or Group in legend.</source>
+        <translation>レイヤパネルのレイヤまたはグループを選択します.</translation>
+    </message>
+    <message>
+        <location filename="../mapswipetool.py" line="121"/>
+        <source>Active layer is &apos;{}&apos;.</source>
+        <translation>アクティブレイヤは {}.</translation>
+    </message>
+    <message>
+        <location filename="../mapswipetool.py" line="130"/>
+        <source>Active group &apos;{}&apos; need at least one item with visible checked</source>
+        <translation>アクティブグループは &apos;{}&apos; 少なくとも1アイテムがチェックされている必要があります.</translation>
+    </message>
+    <message>
+        <location filename="../mapswipetool.py" line="135"/>
+        <source>Active group is &apos;{}&apos;.</source>
+        <translation>アクティブグループは {}.</translation>
+    </message>
+    <message>
+        <location filename="../mapswipetool.py" line="116"/>
+        <source>Active layer &apos;{}&apos; need be a spatial layer.</source>
+        <translation>アクティブレイヤは &apos;{}&apos; 地物レイヤである必要があります.</translation>
+    </message>
+</context>
+</TS>
